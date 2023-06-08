7d215c57070130357295f5c2ac72f852a6c40b40
Adithya <adippb@bu.edu>
2023-06-09 01:03:49 +0530

    FoodDB & Calorie Calculation functionalities.
    I have added the foods and calorieCalculation apps for  the FoodDB & Calorie Calculation functionalities.

---
+++ Food1Proj/calorieCalculation/utils.py
@@ -0,0 +1,4 @@
+def calculate_food_calories(base_calories, servings):
+    # Implement your logic to calculate the calories consumed based on no. of servings
+    # Return the calculated value
+    return base_calories * servings
--- Food1Proj/foodPrj1/settings.py
+++ Food1Proj/foodPrj1/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foods.apps.FoodsConfig'
+    'foods.apps.FoodsConfig',
+    'calorieCalculation.apps.CaloriecalculationConfig'
 ]
 
 MIDDLEWARE = [
