17b7a220b897f6f6673f0bcb44844f10c53d6811
Eldoov <zuowent@bu.edu>
2023-06-08 00:20:13 -0400

    remove the email host password to prevent data breach

--- code/Project/calorietracker/settings.py
+++ code/Project/calorietracker/settings.py
@@ -157,4 +157,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cs673team3@gmail.com'
-EMAIL_HOST_PASSWORD = 'vkfyympkkcobgbnn'
+EMAIL_HOST_PASSWORD = ''
